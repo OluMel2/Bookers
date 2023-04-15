91aab68f91821280239bf3662bf81e7bd1d8d32d
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-04-15 08:59:36 +0000

    [update]課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
   end
 
   def destroy
-  book = Book.find(params[:id])
+  @book = Book.find(params[:id])
   book.destroy
   redirect_to '/books'
   end
